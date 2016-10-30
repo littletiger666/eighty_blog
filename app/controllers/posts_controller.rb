780dbdd762d68293b32bbb08d8a4a73ef4a9cf3d
Xiaohu Li <li.xiaohu6@gmail.com>
2016-10-29 22:54:30 -0700

    Add edit, show and destroy features for Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:update, :show, :destroy, :edit]
 
   def index
   end
@@ -19,7 +20,27 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to 'index'
+  end
+
   private
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
   def post_params
     params.require(:post).permit(:title, :body)
   end
